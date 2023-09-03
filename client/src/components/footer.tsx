9b36de7257cadc216c3fe7ffaddf9a04a00c5573
lucas1visa <visalucas557@gmail.com>
2023-09-03 05:51:28 -0300

    .

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -21,22 +21,17 @@ const FooterPadel = () => {
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                About
+              whatsapp
               </a>
             </li>
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                Licensing
+                Instagram
               </a>
             </li>
             <li>
               <a href="#" className="hover:underline">
-                Contact
+                Facebook
               </a>
             </li>
           </ul>
